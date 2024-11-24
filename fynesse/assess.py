f29c59af4464c0b6a5a423d0528ab07983c7c350
olihec <hectorow@dulwich.org.uk>
2024-11-24 18:07:54 +0000

    fixed bug

--- fynesse/assess.py
+++ fynesse/assess.py
@@ -163,7 +163,7 @@ def get_osm_features_from_codes(connection, oa_codes):
         new_osm_features = ox.features_from_polygon(square_polygon, tags={'amenity': True, 'building': True})
         new_osm_features['OA21CD'] = oa_code
 
-        osm_features = pd.concat([osm_features, new_osm_features], ignore_index=True)
+        osm_features_df = pd.concat([osm_features_df, new_osm_features], ignore_index=True)
 
     
     return osm_features_df

8a6d33e56ed89a4259b1ca52bf289f4797b2ad61
olihec <hectorow@dulwich.org.uk>
2024-11-26 14:59:02 +0000

    attempt to make address visible

--- fynesse/address.py
+++ fynesse/address.py
@@ -1,4 +1,5 @@
 # This file contains code for suporting addressing questions in the data
+from .config import *
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import r2_score, mean_squared_error

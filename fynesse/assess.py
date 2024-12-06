763c1a3ae6b023b071edfbee4ef27ffc4673a414
olihec <hectorow@dulwich.org.uk>
2024-12-06 18:18:19 +0000

    organised files

--- fynesse/assess.py
+++ fynesse/assess.py
@@ -353,19 +353,3 @@ WHERE { ' OR '.join(["tags LIKE '%" + key + "%'" for key in tags_list]) }
 
     return min_distance_km
 
-def data():
-    """Load the data from access and ensure missing values are correctly encoded as well as indices correct, column names informative, date and times correctly formatted. Return a structured data structure such as a data frame."""
-    df = access.data()
-    raise NotImplementedError
-
-def query(data):
-    """Request user input for some aspect of the data."""
-    raise NotImplementedError
-
-def view(data):
-    """Provide a view of the data that allows the user to verify some aspect of its quality."""
-    raise NotImplementedError
-
-def labelled(data):
-    """Provide a labelled set of data ready for supervised learning."""
-    raise NotImplementedError
